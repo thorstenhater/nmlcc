b8a36e91db1b4bc7b34adb13978259b2228d0a3a
Brent Huisman <brent@huisman.pl>
2022-02-14 13:20:09 +0100

    baby steps

--- src/network.rs
+++ src/network.rs
@@ -2,10 +2,32 @@
 
 use tracing::trace;
 
-use crate::{error::Result, expr::Quantity, lems::file::LemsFile, neuroml::raw};
+use crate::{error::Result, expr::Quantity, lems::file::LemsFile, neuroml::raw, Map};
+
+pub struct Population {
+    pub members: Vec<i64>, //list of IDs.
+    pub prototype: String,
+}
+
+
+
 
 pub struct Network {
-    pub temp: f64,
+    pub temperature: f64,
+    pub populations: Map<String,Population>,
+    
+    
+    
+    /* Fill out w stuff from neuroml/raw.rs (Network, Networkbody, Population, PopulationBody)
+
+    3 things important:
+    * populations (=> map)
+    * projections
+    * connections ?
+
+
+
+    */
 }
 
 impl Network {
@@ -13,23 +35,40 @@ impl Network {
         let t = net.temperature.as_deref().unwrap_or("0K");
         let t = Quantity::parse(t)?;
         let t = lems.normalise_quantity(&t)?;
+        let mut populations = Map::new();
 
         use raw::NetworkBody::*;
         for item in &net.body {
             match item {
                 projection(p) => proj(p)?,
-                population(p) => pop(p)?,
+                population(p) => pop(p,&mut populations)?,
                 inputList(i) => {}
                 explicitInput(i) => stim(i)?,
                 _ => trace!("Ignored {:?} in Network", item),
             }
         }
 
-        Ok(Network { temp: t.value })
+        Ok(Network { temperature: t.value,
+                     populations
+                     })
     }
 }
 
-fn pop(p: &raw::Population) -> Result<()> {
+fn pop(p: &raw::Population, ps: &mut Map<String,Population>) -> Result<()> {
+    let mut members = Vec::new();
+    let prototype = p.component.to_string();
+
+    for item in &p.body{
+        match item {
+            instance(i) => member.push(i.id.unwrap()), //id is optional, so may explode.
+            _ => trace!("Ignored {:?} in Population", item),
+        }
+    }
+
+    let pop = Population{ members,
+                          prototype
+                          };
+    ps.insert(p.id.to_string(),pop);
     Ok(())
 }
 
